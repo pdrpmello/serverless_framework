ba6f78cc59eecf042c69f88232faf2309daae9d2
Pedro Mello <150696233+pdrpmello@users.noreply.github.com>
2024-12-18 16:46:30 -0300

    update

--- handler_1.py
+++ handler_1.py
@@ -77,31 +77,58 @@ erp_data = [
     }
 ]
 
-def lambda_handler(event, context):
-    # Tratar e transformar os dados
-    transformed_data = transform_data(erp_data)
+def calcular_desconto(valor, desconto):
+    if desconto is None:
+        return valor
     
-    # Salvar os dados transformados em um bucket S3
-    s3 = boto3.client('s3')
-    bucket_name = 'challenge-bucket737'
-    s3.put_object(Bucket=bucket_name, Key='transformed_data.json', Body=json.dumps(transformed_data))
+    if "%" in desconto:
+        porcentagem = float(desconto.replace('%', '').replace(',', '.')) / 100  # Corrigido
+        return valor * (1 - porcentagem)
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Dados transformados e salvos com sucesso!')
-    }
+    if "R$" in desconto:
+        valor_desconto = float(desconto.replace('R$', '').replace(',', '.'))
+        return valor - valor_desconto
+    
+    return valor
 
+def processar_dados(erp_data):
+    for pedido in erp_data:
+        valor_com_desconto = calcular_desconto(pedido["valor"], pedido["desconto"])
+        pedido["valor_com_desconto"] = round(valor_com_desconto, 2)
+        pedido["total"] = round(valor_com_desconto + pedido["frete"], 2)  # Inclui o frete no total
+    return erp_data
+    
 def transform_data(data):
+    # Processar os dados com cálculo de descontos
+    data_processada = processar_dados(data)
+    
     # Exemplo de transformação: simplificar a estrutura dos dados
     transformed = []
-    for entry in data:
+    for entry in data_processada:
         transformed_entry = {
             "id": entry["id"],
             "valor": entry["valor"],
+            "valor_com_desconto": entry["valor_com_desconto"],  # Adicionando valor com desconto
+            "total": entry["total"],  # Adicionando total
             "data": entry["data"],
             "frete": entry["frete"],
             "desconto": entry["desconto"] if entry["desconto"] else "Nenhum desconto",
             "status": entry["status"]
         }
         transformed.append(transformed_entry)
+    
     return transformed
+
+def lambda_handler(event, context):
+    # Tratar e transformar os dados
+    transformed_data = transform_data(erp_data)
+    
+    # Salvar os dados transformados em um bucket S3
+    s3 = boto3.client('s3')
+    bucket_name = 'flashcrm'
+    s3.put_object(Bucket=bucket_name, Key='transformed_data.json', Body=json.dumps(transformed_data))
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Dados transformados e salvos com sucesso!')
+    }
